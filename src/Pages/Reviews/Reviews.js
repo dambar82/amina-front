2f3175ba592b7a7affcdc99b33469424bf81d948
unknown <killedsparrow@gmail.com>
2025-03-31 18:08:54 +0400

    upd

--- src/Pages/Reviews/Reviews.js
+++ src/Pages/Reviews/Reviews.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import styles from './Reviews.module.scss';
 import multfilmStyles from '../Multfilm/Multfilm.module.scss';
 import axios from "axios";
@@ -9,7 +9,21 @@ const sort_types = [
     {key: 'photo', value: 'Фотографияләр белән', endpoint: 'amina/image_feedbacks'}
 ]
 
+const organization_types = [
+    {key: 'школа', value: 'Мәктәп'},
+    {key: 'детский сад', value: 'Балалар бакчасы'},
+    {key: 'частное лицо', value: 'Шәхси зат'},
+]
+
+
 const url = 'https://api.multfilm.tatar/api/';
+
+function formatName(fullName) {
+    const nameParts = fullName.split(' ');
+    const firstName = nameParts[1];
+    const lastNameInitial = nameParts[0].charAt(0) + '.';
+    return `${firstName} ${lastNameInitial}`;
+}
 
 const Reviews = () => {
 
@@ -19,6 +33,29 @@ const Reviews = () => {
     const [selectActive, setSelectActive] = useState(false);
     const [loading, setLoading] = useState(false);
     const [reviews, setReviews] = useState([]);
+    const [overflowingReviews, setOverflowingReviews] = useState({});
+    const textRef = useRef(null);
+    const [showForm, setShowForm] = useState(false);
+    const [selectOrganizationValue, setSelectOrganizationValue] = useState(organization_types[0]);
+    const [organizationSelectActive, setOrganizationSelectActive] = useState(false);
+    const [districtsSelectActive, setDistrictsSelectActive] = useState(false);
+    const [organizationType, setOrganizationType] = useState(organization_types[0].key);
+    const [districts, setDistricts] = useState([]);
+    const [districtValue, setDistrictValue] = useState(null);
+    const [organizationName, setOrganizationName] = useState('');
+    const [fio, setFio] = useState('');
+    const [job, setJob] = useState('');
+    const [email, setEmail] = useState('');
+
+    const [formData, setFormData] = useState({
+        institution: "Мәктәп",
+        city: "Казань",
+        name: "",
+        job: "",
+        email: "",
+        experience: "",
+        file: null,
+    });
 
     const updateImage = () => {
 
@@ -48,53 +85,235 @@ const Reviews = () => {
         fetchReviews();
     }, [sortType]);
 
+    useEffect(() => {
+        const fetchDistricts = async () => {
+            const response = await axios.get(`${url}amina/districts`);
+            console.log('disctricts', response.data);
+            setDistricts(response.data)
+            setDistrictValue(response.data[0])
+        }
+        fetchDistricts()
+    }, [])
+
     const handleSelectValue = ( elem ) => {
         setSelectValue(elem)
         setSelectActive(false)
         setSortType(elem.key)
     }
 
+    const handleSelectOrganization = ( elem ) => {
+        setSelectOrganizationValue(elem)
+        setOrganizationSelectActive(false);
+        setOrganizationType(elem.key)
+    }
+
+    const handleSelectDistrict = ( elem ) => {
+        setDistrictValue(elem);
+        setDistrictsSelectActive(false);
+    }
+
+    const [expandedReviews, setExpandedReviews] = useState({});
+
+    const toggleExpand = (id) => {
+        setExpandedReviews((prev) => ({
+            ...prev,
+            [id]: !prev[id], // Переключаем состояние конкретной песни
+        }));
+    };
+
+    useEffect(() => {
+        const checkOverflow = () => {
+            const newOverflowingReviews = {};
+            reviews.forEach(review => {
+                const textElement = document.getElementById(`review-text-${review.id}`);
+                if (textElement) {
+                    const prevMaxHeight = textElement.style.maxHeight;
+                    textElement.style.maxHeight = "none"; // Убираем ограничение
+                    const fullHeight = textElement.scrollHeight;
+                    textElement.style.maxHeight = prevMaxHeight; // Возвращаем обратно
+                    newOverflowingReviews[review.id] = fullHeight > 110;
+                }
+            });
+            setOverflowingReviews(newOverflowingReviews);
+        };
+
+        checkOverflow();
+    }, [reviews]);
+
     return (
-        <div className={'pageWrapper'}>
-            <div className={'pageHeaderImage'}>
-                <img src={imgSrc} alt=""/>
-                <span>Бәяләмәләр</span>
-            </div>
-            <div className={'pageContent'}>
-                <div className={styles.reviews}>
-                    <div className={styles.reviews_header}>
-                        <button className={`${styles.button} ${styles.button_pink}`}>
-                            Оставить отзыв
-                        </button>
-                        <div className={styles.sort}>
-                            <div
-                                className={`${styles.sort_value} ${selectActive ? styles._active : ''}`}
-                                onClick={() => setSelectActive(prev => !prev)}
-                            >
-                                <span>{selectValue.value}</span>
-                                <img src="./img/tudasuda.svg" alt=""/>
-                            </div>
-                            <div className={styles.sort_select}>
-                                {sort_types.map(elem => (
-                                    <div
-                                        key={elem.key}
-                                        onClick={() => handleSelectValue(elem)}
-                                        className={elem.key === selectValue.key ? styles._active : ''}
-                                    >
-                                        {elem.value}
+        <>
+            {showForm && (
+                <div className={styles.overlay} onClick={() => setShowForm(false)}>
+                    <div className={styles.reviewForm} onClick={(e) => e.stopPropagation()}>
+                        <h1>Фикер калдырырга</h1>
+                        <form action="">
+                            <div className={styles.inputs}>
+                                <div className={styles.grid}>
+                                    <div className={styles.select}>
+                                        <label className={styles.label}>Оешма төре</label>
+                                        <div className={styles.select_wrapper}>
+                                            <div className={`${styles.value} ${organizationSelectActive ? styles._active : ''}`}
+                                                onClick={() => {setOrganizationSelectActive(prev => !prev); setDistrictsSelectActive(false)}}
+                                            >
+                                                <span>{selectOrganizationValue.value}</span>
+                                            </div>
+                                            <div className={styles.select_values}>
+                                                {organization_types.map(elem => (
+                                                    <div
+                                                        key={elem.key}
+                                                        onClick={() => handleSelectOrganization(elem)}
+                                                        className={elem.key === selectOrganizationValue.key ? styles._active : ''}
+                                                    >
+                                                        {elem.value}
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        </div>
                                     </div>
-                                ))}
+                                    <div className={styles.select}>
+                                        <label className={styles.label}>Төбәк</label>
+                                        <div className={styles.select_wrapper}>
+                                            <div className={`${styles.value} ${districtsSelectActive ? styles._active : ''}`}
+                                                 onClick={() => {setDistrictsSelectActive(prev => !prev); setOrganizationSelectActive(false)}}
+                                            >
+                                                <span>{districtValue.title}</span>
+                                            </div>
+                                            <div className={styles.select_values}>
+                                                {districts.map(elem => (
+                                                    <div
+                                                        key={elem.title}
+                                                        onClick={() => handleSelectDistrict(elem)}
+                                                        className={elem.title === districtValue.title ? styles._active : ''}
+                                                    >
+                                                        {elem.title}
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                {organizationType !== 'частное лицо' && (
+                                    <div className={styles.input_wrapper}>
+                                        <label>Учреждение исеме</label>
+                                        <div className={styles.input_custom}>
+                                            <input
+                                                type="text"
+                                                placeholder='Мәктәп/лицей исеме'
+                                                onChange={(e) => setOrganizationName(e.target.value)}
+                                            />
+                                        </div>
+                                    </div>
+                                )}
+                                <div className={styles.grid}>
+                                    <div className={styles.input_wrapper}>
+                                        <label>ФИО</label>
+                                        <div className={styles.input_custom}>
+                                            <input
+                                                type="text"
+                                                placeholder='Исемегезне яки тулы исем-фамилиягезне күрсәтегез'
+                                                onChange={(e) => setFio(e.target.value)}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className={styles.input_wrapper}>
+                                        <label>Вазыйфа</label>
+                                        <div className={styles.input_custom}>
+                                            <input
+                                                type="text"
+                                                placeholder='Мәсәлән: Укытучы, Тәрбияче'
+                                                onChange={(e) => setJob(e.target.value)}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={styles.input_wrapper}>
+                                    <label>Почта</label>
+                                    <div className={styles.input_custom}>
+                                        <input
+                                            type="email"
+                                            placeholder='Электрон почта'
+                                            onChange={(e) => setEmail(e.target.value)}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            )}
+            <div className={'pageWrapper'}>
+                <div className={'pageHeaderImage'}>
+                    <img src={imgSrc} alt=""/>
+                    <span>Бәяләмәләр</span>
+                </div>
+                <div className={'pageContent'}>
+                    <div className={styles.reviews}>
+                        <div className={styles.reviews_header}>
+                            <button className={`${styles.button} ${styles.button_pink}`} onClick={() => setShowForm(true)}>
+                                Оставить отзыв
+                            </button>
+                            <div className={styles.sort}>
+                                <div
+                                    className={`${styles.sort_value} ${selectActive ? styles._active : ''}`}
+                                    onClick={() => setSelectActive(prev => !prev)}
+                                >
+                                    <span>{selectValue.value}</span>
+                                    <img src="./img/tudasuda.svg" alt=""/>
+                                </div>
+                                <div className={styles.sort_select}>
+                                    {sort_types.map(elem => (
+                                        <div
+                                            key={elem.key}
+                                            onClick={() => handleSelectValue(elem)}
+                                            className={elem.key === selectValue.key ? styles._active : ''}
+                                        >
+                                            {elem.value}
+                                        </div>
+                                    ))}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className={styles.reviews_list}>
-                        {[...reviews, ...reviews].map(review => (
-                            <div key={review.id} className={styles.reviews_card}>
-                                <h3>{review.organization}</h3>
-                                <p>{review.text}</p>
-                                {review.images && <img src={review.images[0]} alt="Отзыв" />}
-                            </div>
-                        ))}
+                        <div className={styles.reviews_list}>
+                            {reviews.map(review => (
+                                <div key={review.id} className={styles.reviews_card}>
+                                    <div className={styles.cardHeader}>
+                                        <div className={styles.avatar}>
+                                            <img src={review.creator === "частное лицо" ? './img/personPic.png' : review.creator === "школа" ? './img/schoolPic.png' : review.creator === "организация" && './img/sadikPic.png'} alt=""/>
+                                        </div>
+                                        <div className={styles.textPart}>
+                                            <div className={styles.name}>
+                                                <div className={styles.fio}>
+                                                    {formatName(review.fio)}
+                                                </div>
+                                                <div className={styles.org}>
+                                                    {review.organization? review.organization : 'Частное лицо'}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className={styles.content}>
+                                        <div
+                                            ref={textRef}
+                                            className={`${styles.text} ${expandedReviews[review.id] ? styles.expanded : ""}`}>{review.text}</div>
+                                        {(overflowingReviews[review.id] || expandedReviews[review.id]) && (
+                                            <div
+                                                className={styles.showMoreButton}
+                                                onClick={() => toggleExpand(review.id)}
+                                            >
+                                                {expandedReviews[review.id] ? "Төреп куярга" : "Тулысынча күрсәтергә"}
+                                            </div>
+                                        )}
+                                        {review.images?.length > 0 && (
+                                            <div className={`${styles.images} ${review.images.length === 1 ? styles.singleImage : styles.multipleImages}`}>
+                                                {review.images.map((image, index) => (
+                                                    <img key={index} src={image} alt="Отзыв" />
+                                                ))}
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -106,7 +325,7 @@ const Reviews = () => {
                     © 2024 «Әминә». Барлык хокуклар якланган. Сайт материалларын язма рөхсәт белән генә файдаланырга ярый.
                 </p>
             </div>
-        </div>
+        </>
     );
 };
 
