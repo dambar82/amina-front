b9665e22863b24575302eda82074eb25b7c24b1c
unknown <killedsparrow@gmail.com>
2025-04-01 15:53:12 +0400

    scroll to top

--- src/Pages/Songs/Songs.js
+++ src/Pages/Songs/Songs.js
@@ -130,9 +130,42 @@ const Songs = () => {
         }
     };
 
+    const [isScrolledHalf, setIsScrolledHalf] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const halfHeight = window.innerHeight / 2;
+            if (window.scrollY > halfHeight) {
+                setIsScrolledHalf(true);
+            } else {
+                setIsScrolledHalf(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+
     return (
         <>
             <div className={'pageWrapper'}>
+                {
+                    isScrolledHalf && (
+                        <div className={'scrollToTop'} onClick={scrollToTop}>
+                            <img src="./img/toTopBlack.svg" alt=""/>
+                        </div>
+                    )
+                }
                 <div className={'pageHeaderImage'}>
                     <img src={imgSrc} alt=""/>
                     <span>Җырлар</span>
