4ac3cb1b48781c48d611c2846945809ddfa54495
daniel <killedsparrow@gmail.com>
2026-09-04 12:14:56 +0400

    upd

--- src/Pages/Multfilm/MultfilmPage.js
+++ src/Pages/Multfilm/MultfilmPage.js
@@ -143,7 +143,7 @@ const MultfilmPage = () => {
                             <div className={'modalContent'}>
                                 <span className={'close'} onClick={closeModal}>&times;</span>
                                 <video controls onClick={(event) => event.stopPropagation()}>
-                                    <source src={selectedVideo.link} type="video/mp4" />
+                                    <source src={selectedVideo.video} type="video/mp4" />
                                     Your browser does not support the video tag.
                                 </video>
                             </div>
